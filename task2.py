51d2765c3bb1b64191c240c40126d0559398d939
Ivan Ushachov <ushachovg324@gmail.com>
2023-12-12 23:04:53 +0200

    add comments

--- task2.py
+++ task2.py
@@ -1,3 +1,8 @@
+# Користувач вводить шестизначне число. Необхідно поміняти в цьому числі першу та
+# шосту цифри, а також другу та п'яту цифри.
+# Наприклад, 723895 має перетворитися на 593827
+# Якщо користувач ввів не шестизначне число, потрібно вивести повідомлення про помилку.
+
 number = int(input("Enter six-digit number: "))
 
 # Check for six digit number
@@ -13,5 +18,5 @@ digit_4 = (number // 100) % 10
 digit_5 = (number // 10) % 10
 digit_6 = number % 10
 
-# Swap places 1-6 and 2-5 numbers
+# Swap places 1-6 and 2-5
 print(digit_6 * 100000 + digit_5 * 10000 + digit_3 * 1000 + digit_4 * 100 + digit_2 * 10 + digit_1 * 1 )
